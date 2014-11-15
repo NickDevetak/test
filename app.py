13659858100c864762b8564dc45f449b918967f7
Nicolas Devetak <nicolasdevetak@gds3619.home>
2014-11-15 21:38:40 +0000

    more stuff

--- app.py
+++ app.py
@@ -1,6 +1,7 @@
-from flask import Flask
+from flask import Flask, request
 from flask.ext.sqlalchemy import SQLAlchemy
 import os
+import json
 
 app = Flask(__name__)
 app.config.from_object(os.environ['APP_SETTINGS'])
@@ -21,6 +22,27 @@ def hello():
 @app.route('/<name>')
 def hello_name(name):
     return "Hello {}!".format(name)
+
+@app.route('/upload', methods=['POST'])
+def upload_results():
+
+    json_text = request.get_json()
+    print json_text
+
+    json_object = request.data
+    print json_object
+
+    for i in json_text['results']:
+        print i['feature']
+        print i['run_time']
+        result = Result(
+            feature=i['feature'],
+            run_time=i['run_time'],
+            )
+        db.session.add(result)
+        db.session.commit()
+
+    return "Success"
 
 if __name__ == '__main__':
     app.run()
